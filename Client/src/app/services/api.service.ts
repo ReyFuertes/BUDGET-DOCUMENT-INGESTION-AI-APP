357d4d1ddec577f99ad19d20381b90e6231adba0
Rey Fuertes <reynelfuertes@gmail.com>
2025-12-08 21:54:43 +0800

    refactor(api): make service stateless for ngrx integration

--- Client/src/app/services/api.service.ts
+++ Client/src/app/services/api.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { Receipt } from '../models/receipt';
 import { environment } from '../../environments/environment';
 import { BaseService } from './base.service';
@@ -11,28 +10,15 @@ import { BaseService } from './base.service';
 export class ApiService extends BaseService {
   private baseUrl = environment.baseUrl;
 
-  receipts = signal<Receipt[]>([]);
-
-  constructor() {
-    super();
-    this.fetchReceipts();
-  }
-
-  fetchReceipts() {
-    this.get<Receipt[]>(this.baseUrl).subscribe(data => {
-      this.receipts.set(data);
-    });
+  getReceipts(): Observable<Receipt[]> {
+    return this.get<Receipt[]>(this.baseUrl);
   }
 
   uploadReceipt(file: File): Observable<Receipt> {
     const formData = new FormData();
     formData.append('file', file);
 
-    return this.post<Receipt>(`${this.baseUrl}/upload`, formData).pipe(
-      tap(newReceipt => {
-        this.receipts.update(current => [...current, newReceipt]);
-      })
-    );
+    return this.post<Receipt>(`${this.baseUrl}/upload`, formData);
   }
 
   downloadReport(): void {
